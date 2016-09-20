dc11a5d43235ec133d493926f8532202810e125b
Orka <orkalittlecutewolf@live.com>
2016-09-20 19:49:45 +0200

    Quasi finalisation de la page newAccount
    La page de création de compte est connectée à la bdd et certains tests sont réalisés.

--- newAccount.php
+++ newAccount.php
@@ -45,9 +45,35 @@
         $newUserMail = htmlspecialchars($_POST['userMail']);
         $newUserLang = htmlspecialchars($_POST['preferredLangage']);
 
-        echo"YO". $newUserLogin . $newUserPwd . $newUserPwdConfirm. $newUserMail . $newUserLang . " et voilà !!";
+        // on demande à php de tenter la connexion à la bdd via PDO si les 2 mots de passe saisis sont identiques sinon...
+        try {
+        if($newUserPwd == $newUserPwdConfirm && $newUserLogin !== $newUserPwd) {
+          // on se connecte à la base de données avec l'objet PDO
+          $bdd = new PDO('mysql:host=localhost;dbname=jdrformu;charset=utf8', 'root', '');
 
+          // on prépare la requête (création d'un compte utilisateur)
+          $reqCreateAccount = $bdd->prepare('INSERT INTO membres(login, password, email, language) VALUES(:login, :password, :email, :language)');
+          // on passe un tableau contenant les infos submitted par l'user pour pouvoir exécuter la requête
+          $reqCreateAccount->execute(array(
+            'login' => $newUserLogin,
+            'password' => $newUserPwd,
+            'email' => $newUserMail,
+            'language' => $newUserLang
+          ));
 
+          // on affiche un message pour signifier le succès de la création de compte à l'utilisateur/ice.
+          echo"SUCCESS ! <br> Le compte utilisateur a bien été créé !";
+          // on vérifie que le login n'est pas identique au mot de passe
+        } elseif ($newUserLogin == $newUserPwd) {
+          echo "Votre mot de passe doit être différent de votre login !";
+        } else {
+          // ou un message d'erreur.
+          echo "Vous devez saisir 2 fois le même mot de passe !";
+            }
+          // on affiche un message d'erreur pour l'utilisateur/rice s'il y a eu une erreur lors de l'exécution.
+          } catch(Exception $erreur) {
+            die("Malheureusement, nous avons rencontré une erreur : ".$erreur->getMessage());
+          }
       }
      ?>
 
