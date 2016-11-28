4f790d6c6463fd0c5f006b995b0a05291373798b
Orka <orkalittlecutewolf@live.com>
2016-11-28 23:13:31 +0100

    CSS appliqué sur la page logout
     - Le visuel de la page logout est fait.

--- logout.php
+++ logout.php
@@ -10,17 +10,22 @@
     <meta content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta lang="fr">
-    <title>Logout</title>
+    <title>Déconnexion</title>
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="CSS/style.css">
 </head>
 
 
 <body>
-  <section class="container-fluid">
-    <div class="row">
-      <h3 class="col-lg-12 text-center">Vous avez été logout</h3>
-      <a href="index.php" class="col-lg-12">retour vers l'accueil</a>
+  <section class="container-fluid navigation">
+    <?php
+      include('header.php');
+    ?>
+  </section>
+  <section class="container text-center">
+    <div class="row login-component">
+      <h1 class="col-lg-12 col-md-12 col-sm-12 col-xs-12">Déconnexion</h1>
+      <p class="col-lg-12 col-md-12 col-sm-12 col-xs-12">Vous avez bien été déconnecté-e.</p>
     </div>
   </section>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
