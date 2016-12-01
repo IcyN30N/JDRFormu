833e3c28885736ccefcaa995f916eeb589c43da7
Orka <orkalittlecutewolf@live.com>
2016-12-01 18:20:57 +0100

    Modification du contenu de la navigation
    Si l'utilisateur-ice est connecté-e, la navigation compte maintenant 6 éléments.

--- header.php
+++ header.php
@@ -1,19 +1,22 @@
 <?php
   if(isset($_SESSION['login'])) {
 ?>
-    <nav class="row text-center">
-      <a class="col-lg-2 col-md-2 col-sm-2 col-xs-12" href="index.php">Accueil</a>
-      <a class="col-lg-3 col-xs-12"href="autoGenerator.php">AutoGen</a>
-      <a class="col-lg-3 col-xs-12" href="changeUserSettings.php">Modifier les paramètres utilisateurs</a>
-      <a class="col-lg-3 col-xs-12" href="logout.php">Se déconnecter</a>
-      <a class="col-lg-1 col-xs-12 text-right"><?php echo $_SESSION['login'] ?></a>
-    </nav>
+  <nav class="row text-center">
+    <a class="col-lg-2 col-md-2 col-sm-2 col-xs-12" href="about.php">Accueil</a>
+    <a class="col-lg-2 col-md-2 col-sm-2 col-xs-12"href="autoGenerator.php">Générateur</a>
+    <a class="col-lg-2 col-md-2 col-sm-2 col-xs-12"href="liste.php">Liste</a>
+    <a class="col-lg-2 col-md-2 col-sm-2 col-xs-12" href="changeUserSettings.php">Paramètres</a>
+    <a class="col-lg-2 col-md-2 col-sm-2 col-xs-12" href="logout.php">Déconnexion</a>
+    <a class="col-lg-2 col-md-2 col-sm-2 col-xs-12"><?php echo $_SESSION['login'] ?></a>
+  </nav>
+
+
 <?php
   } else {
 ?>
     <nav class="row text-center">
-      <a class="col-lg-3 col-md-3 col-sm-3 col-xs-12" href="index.php">Accueil</a>
-      <a class="col-lg-3 col-md-3 col-sm-3 col-xs-12" href="autoGenerator.php">AutoGen</a>
+      <a class="col-lg-3 col-md-3 col-sm-3 col-xs-12" href="about.php">Accueil</a>
+      <a class="col-lg-3 col-md-3 col-sm-3 col-xs-12" href="autoGenerator.php">Générateur</a>
       <a class="col-lg-3 col-md-3 col-sm-3 col-xs-12" href="newAccount.php">S'inscrire</a>
       <a class="col-lg-3 col-md-3 col-sm-3 col-xs-12" href="login.php">Se connecter</a>
     </nav>
